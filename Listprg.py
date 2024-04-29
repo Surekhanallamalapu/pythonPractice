5e24dfe83e74517993f7956795b8932b632891a0
Surekhanallamalapu <143636431+Surekhanallamalapu@users.noreply.github.com>
2024-04-29 14:52:51 +0530

    cmt

---
+++ Listprg.py
@@ -0,0 +1,7 @@
+a=[1,2,3,4,5,6]
+b=a.copy()
+a.append(7)
+print(a)
+print(b)
+values = [[1, 'i', 'a'], ['2', 'we', 'be', 'it'], [3, 'are', 'few', 'too']]
+list(range(101))[::-5]
---
+++ setprg.py
@@ -0,0 +1,8 @@
+myset={2,3,4,2,3,'name'}
+myset.add(5)
+myset.add(8)
+myset.add('suresh')
+myset.add(7)
+myset.add(0)
+myset.add('sure')
+print(myset)
